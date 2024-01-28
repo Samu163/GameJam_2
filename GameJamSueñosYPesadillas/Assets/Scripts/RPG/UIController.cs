b1a5742ea544f0c03c9cebdc381f6d79604b4772
D1dii <didacgp123@gmail.com>
2024-01-28 22:41:33 +0100

    Activar y desactivar menu items

--- GameJamSueñosYPesadillas/Assets/Scripts/RPG/UIController.cs
+++ GameJamSueñosYPesadillas/Assets/Scripts/RPG/UIController.cs
@@ -5,11 +5,30 @@ using UnityEngine;
 public class UIController : MonoBehaviour
 {
     public ItemManager itemInventoryPrefabRef;
+    public bool isMenuActive = false;
+    public bool isMenuCreated = false;
+
+    private ItemManager inventory;
 
     public void itemButton()
     {
-        var inventory = Instantiate(itemInventoryPrefabRef, transform);
-        inventory.transform.position = new Vector3(200, 50, 0);
+        
+        if(!isMenuCreated)
+        {
+            inventory = Instantiate(itemInventoryPrefabRef, transform);
+            inventory.transform.position = new Vector3(200, 50, 0);
+            isMenuCreated = true;
+        }
 
+        if(!isMenuActive)
+        {
+            inventory.gameObject.SetActive(true);
+        } 
+        else if (isMenuActive)
+        {
+            inventory.gameObject.SetActive(false);
+        }
+        isMenuActive = !isMenuActive;
+        
     }
 }
